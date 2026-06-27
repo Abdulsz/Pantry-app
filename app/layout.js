2752a461d2aec1a33a81440a6f16d9c229be2538
Abdul-Salam Zakaria <151104581+Abdulsz@users.noreply.github.com>
2026-06-27 18:33:01 -0400

    update: app/layout.js - Add dark mode to the home page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,26 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#DD5349',
+    },
+    background: {
+      default: '#ffffff',
+      paper: '#ffffff',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#333333',
+    },
+  },
+});
 
 export const metadata = {
   title: "Pantry Tracker",
@@ -11,7 +30,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
