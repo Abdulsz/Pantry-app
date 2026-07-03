6ae2132f8e3e555ba60ae0874002767d8d6fba11
Abdul-Salam Zakaria <151104581+Abdulsz@users.noreply.github.com>
2026-07-03 14:12:00 -0400

    update: app/page.js - Add dark mode to the home page

--- app/page.js
+++ app/page.js
@@ -485,18 +485,18 @@ export default function Home() {
                   }}
                 />
                 <Box p={2}>
-                  <Typography variant="h5" color="#333" textAlign="center">
+                  <Typography variant="h5" color={darkMode ? "#fff" : "#333"} textAlign="center">
                     {name.charAt(0).toUpperCase() + name.slice(1)}
                   </Typography>
 
-                  <Typography variant="h5" color="#333" textAlign="center">
+                  <Typography variant="h5" color={darkMode ? "#fff" : "#333"} textAlign="center">
                     {quantity}
                   </Typography>
 
                   <Button
                     sx={{
-                      backgroundColor: "black",
-                      color: "white",
+                      backgroundColor: darkMode ? "#ffffff" : "black",
+                      color: darkMode ? "black" : "white",
                       "&:hover": {
                         backgroundColor: "#333333",
                       },
